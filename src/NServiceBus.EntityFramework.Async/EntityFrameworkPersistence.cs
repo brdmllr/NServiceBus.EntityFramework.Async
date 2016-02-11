cd6420677c2fbfbcd1fdb56380c1368fff3f4f3f
Brad Miller <brad@bmmiller.com>
2016-02-10 22:02:53 -0500

    Initial solution/project structure.

---
+++ src/NServiceBus.EntityFramework.Async/EntityFrameworkPersistence.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="EntityFrameworkPersistence.cs" company="NServiceBus.EntityFramework.Async">
+// Copyright 2016 Brad M. Miller
+// </copyright>
+
+namespace NServiceBus.EntityFramework.Async
+{
+    using System;
+    using Persistence;
+
+    /// <summary>
+    /// Entry point for Entity Framework persistence.
+    /// </summary>
+    public class EntityFrameworkPersistence : PersistenceDefinition
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EntityFrameworkPersistence"/> class.
+        /// </summary>
+        public EntityFrameworkPersistence()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
